12bd25da8ef063234293ef6e2e639178ecd2b685
RBJCTNN29 <62948217+RBJCTNN29@users.noreply.github.com>
2023-01-24 11:41:52 +0100

    Ex4 updated

--- Ex4/Sprint1_T2_Ex4.php
+++ Ex4/Sprint1_T2_Ex4.php
@@ -1,12 +1,11 @@
 <?php
-    $int_x = 1;
+    $int_x = 3;
     $int_y = 2;
     $dbl_n = 1.5;
     $dbl_m = 2.5;
-    echo $int_x, $int_y, ($int_x + $int_y), 
-        ($int_x - $int_y), ($int_x * $int_y), 
-        ($int_x % $int_y);
-    echo (2 * $dbl_n), (2 * $dbl_m),
-        ($int_x + $int_y + $dbl_n + $dbl_m),
+    echo $int_x . "<br/>" . $int_y . "<br/>" . ($int_x + $int_y) . "<br/>" . 
+        ($int_x - $int_y) . "<br/>" . ($int_x * $int_y) . "<br/>" . 
+        ($int_x % $int_y) . "<br/>" . (2 * $dbl_n) . "<br/>" . (2 * $dbl_m) . "<br/>" .
+        ($int_x + $int_y + $dbl_n + $dbl_m) . "<br/>" .
         ($int_x * $int_y * $dbl_n * $dbl_m);
 ?>
